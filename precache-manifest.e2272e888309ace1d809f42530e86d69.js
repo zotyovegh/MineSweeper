38f8f9ee62bf949c4775f7f753a349b811ce3767
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-06-20 01:36:51 +0200

    Updates

---
+++ precache-manifest.e2272e888309ace1d809f42530e86d69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c103d097d88b1527f86b4810689b4c6",
+    "url": "/MineSweeper/index.html"
+  },
+  {
+    "revision": "48b05cc7a8d20e539b0b",
+    "url": "/MineSweeper/static/css/main.794c9f87.chunk.css"
+  },
+  {
+    "revision": "a4e88b40815776e6486f",
+    "url": "/MineSweeper/static/js/2.2412ca34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MineSweeper/static/js/2.2412ca34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b05cc7a8d20e539b0b",
+    "url": "/MineSweeper/static/js/main.a289bcda.chunk.js"
+  },
+  {
+    "revision": "d50ecaf7c222ba0e37ad",
+    "url": "/MineSweeper/static/js/runtime-main.26b9235d.js"
+  }
+]);
