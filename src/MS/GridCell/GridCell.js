3c99bacb041b2de1598f49dafe290644aed55774
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-06-29 22:53:12 +0200

    Cell created, not functioning yet

---
+++ src/MS/GridCell/GridCell.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GridCell = (props) => {
+  return (
+  <div>
+
+  </div>
+  );
+};
+
+export default GridCell;
--- src/MS/GridRow/GridRow.js
+++ src/MS/GridRow/GridRow.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import GridCell from "../GridCell/GridCell"
 
 const GridRow = (props) => {
-  console.log(props);
+  let cells = props.cells.map((data, index) => {
+    return <GridCell key={index} data={data} />;
+  });
 
-  return <div></div>;
+  return <div className="row">{cells}</div>;
 };
 
 export default GridRow;
