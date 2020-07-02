75958492f060ba89810c4def544353c00db73bf8
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-07-02 22:41:30 +0200

    Cell click handling
    Counting neighboring bombs follows

--- src/App.js
+++ src/App.js
@@ -5,23 +5,33 @@ import Header from "./MS/Header/Header";
 
 class App extends Component {
   state = {
+    openedCells: 0,
     time: 0,
     rows: 10,
     columns: 10,
     mines: 10,
     flags: 10,
+    game: "pending", //Can be pending, running and ended
+  };
+
+  handleCellInspect = () => {
+    if (this.state.game !== "running" && this.state.openedCells === 0) {
+      this.setState({
+        game = "running"
+      })
+    }
   };
 
   render() {
     return (
       <div className="minesweeper">
-        <Header 
-        time={this.state.time} 
-        flags={this.state.flags} />
+        <Header time={this.state.time} flags={this.state.flags} />
         <Grid
+          openedCells={this.openedCells}
           rows={this.state.rows}
           columns={this.state.columns}
           mines={this.state.mines}
+          handleCellInspect={this.state.handleCellInspect}
         />
       </div>
     );
--- src/MS/Grid/Grid.js
+++ src/MS/Grid/Grid.js
@@ -45,12 +45,37 @@ class Grid extends Component {
 
   render() {
     //Create rows
-    
+
     let rows = this.state.rows.map((row, index) => {
       return <GridRow cells={row} key={index} />;
     });
     return <div className="grid">{rows}</div>;
   }
+
+  try = (cell) => {
+    //Display the number of neighboriing bombs or the bomb
+
+    let rows = this.state.rows;
+    let currentCell = rows[cell.x][cell.y];
+
+    if (currentCell.hasMine && this.props.openedCells === 0) {
+      console.log("cell already has mine, restart");
+      let newRows = this.createGrid(this.props);
+      this.setState(
+        {
+          rows: newRows,
+        },
+        () => {
+          this.inspectCell(cell);
+        }
+      );
+    } else {
+      if (!currentCell.isPressed && !cell.hasFlag) {
+        this.props.handleCellInspect();
+        currentCell.isPressed = true;
+      }
+    }
+  };
 }
 
 export default Grid;
