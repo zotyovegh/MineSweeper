f800aeb55f6be5e904af6b79299999a7c177e213
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-07-03 14:48:27 +0200

    Small D: Cell edges fixed

--- src/MS/Cell/Cell.js
+++ src/MS/Cell/Cell.js
@@ -4,7 +4,7 @@ const Cell = (props) => {
   let cell = () => {
     if (props.data.isPressed) {
       return (
-        <div className="press cell" onClick={() => props.tryPress(props.data)}></div>
+        <div className="pressed cell" onClick={() => props.tryPress(props.data)}></div>
       );
     } else {
       return <div className="cell" onClick={() => props.tryPress(props.data)}></div>;
