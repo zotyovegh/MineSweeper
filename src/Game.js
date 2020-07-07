cdde58bcedec02e010188e7dce11f0790dcd44b8
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-07-07 19:08:29 +0200

    Renaming files

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,108 @@
 import React, { Component } from "react";
 import "./index.css";
-import Minesweeper from "./Minesweeper";
+import Grid from "./Game/Grid";
+import Header from "./Game/Header";
+
 class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      openedCells: 0,
+      time: 0,
+      rows: 10,
+      columns: 10,
+      mines: 15,
+      flags: 15,
+      game: "pending", //Can be pending, running, ended and won
+    };
+
+    this.savedState = this.state;
+  }
+
+  componentWillMount() {
+    this.intervals = [];
+  }
+
+  tick = () => {
+    if (this.state.openedCells > 0 && this.state.game === "running") {
+      let time = this.state.time + 1;
+      this.setState({ time });
+    }
+  };
+
+  setInterval = (fn, t) => {
+    this.intervals.push(setInterval(fn, t));
+  };
+
+  componentDidUpdate() {
+    if (this.state.game === "running") {
+      this.winning();
+    }
+  }
+
+  reset = () => {
+    this.intervals.map(clearInterval);
+    this.setState({ ...this.savedState }, () => {
+      this.intervals = [];
+    });
+  };
+
+  finishGame = () => {
+    this.setState({
+      game: "ended",
+    });
+  };
+
+  handleCellInspect = () => {
+    if (this.state.game !== "running" && this.state.openedCells === 0) {
+      this.setState(
+        {
+          game: "running",
+        },
+        () => {
+          this.setInterval(this.tick, 1000);
+        }
+      );
+    }
+
+    this.setState((previousState) => {
+      return { openedCells: previousState.openedCells + 1 };
+    });
+  };
+
+  changeFlagsNumber = (amount) => {
+    this.setState({ flags: this.state.flags + amount });
+  };
+
+  winning = () => {
+    if (
+      this.state.mines + this.state.openedCells >=
+      this.state.columns * this.state.rows
+    ) {
+      this.setState({
+        game: "won",
+      });
+    }
+  };
+
   render() {
     return (
-      <div className="game">
-        <Minesweeper />
+      <div className="minesweeper">
+        <Header
+          time={this.state.time}
+          flags={this.state.flags}
+          reset={this.reset}
+        />
+        <Grid
+          game={this.state.game}
+          openedCells={this.state.openedCells}
+          rows={this.state.rows}
+          columns={this.state.columns}
+          mines={this.state.mines}
+          changeFlagsNumber={this.changeFlagsNumber}
+          onCellInspect={this.handleCellInspect}
+          finishGame={this.finishGame}
+        />
       </div>
     );
   }
---
+++ src/Game/Cell/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+const Cell = (props) => {
+  let cell = () => {
+    if (props.data.isPressed) {
+      if (props.data.hasMine) {
+        return (
+          <div
+            className="bomb cell"
+            onClick={() => props.tryPress(props.data)}
+            onContextMenu={(e) => {
+              e.preventDefault();
+            }}
+          ></div>
+        );
+      } else if (props.data.minesAround === 0) {
+        return (
+          <div
+            className="pressed cell"
+            onClick={() => props.tryPress(props.data)}
+            onContextMenu={(e) => {
+              e.preventDefault();
+              props.flag(props.data);
+            }}
+          />
+        );
+      } else {
+        return (
+          <div
+            className="pressed cell"
+            onClick={() => props.tryPress(props.data)}
+            onContextMenu={(e) => {
+              e.preventDefault();
+              props.flag(props.data);
+            }}
+          >
+            {props.data.minesAround}
+          </div>
+        );
+      }
+    } else if (props.data.hasFlag) {
+      return (
+        <div
+          className="flagged cell"
+          onClick={() => props.tryPress(props.data)}
+          onContextMenu={(e) => {
+            e.preventDefault();
+            props.flag(props.data);
+          }}
+        ></div>
+      );
+    } else {
+      return (
+        <div
+          className="cell"
+          onClick={() => props.tryPress(props.data)}
+          onContextMenu={(e) => {
+            e.preventDefault();
+            props.flag(props.data);
+          }}
+        />
+      );
+    }
+  };
+  return cell();
+};
+
+export default Cell;
