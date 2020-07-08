173487faf669569e78c4741ca733655a3ce99554
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-07-09 00:05:39 +0200

    Rename dialog.js to index

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Game from "../../minesweeper/src/Game";
-import Dialog from "./Dialog/Dialog";
+import Dialog from "./Dialog";
 import "./index.css";
 
 class Minesweeper extends Component {
@@ -9,7 +9,7 @@ class Minesweeper extends Component {
     rows: 10,
     columns: 10,
     mines: 15,
-    flags: 15
+    flags: 15,
   };
 
   changeDialog = () => {
@@ -20,21 +20,27 @@ class Minesweeper extends Component {
 
   onNewGame = () => {
     this.changeDialog();
+    console.log("heyy");
+    this.setState({ rows: 20 });
   };
 
   render() {
     return (
       <div>
         <button onClick={this.changeDialog}>Game</button>
-        <div>
-          <Dialog
-            isOpen={this.state.isOpen}
-            onClose={(e) => this.setState({ isOpen: false })}
-            onNewGame={this.onNewGame}
-          ></Dialog>
-        </div>
 
-        <Game rows={this.state.rows} columns={this.state.columns} mines={this.state.mines} flags={this.state.flags} />
+        <Dialog
+          isOpen={this.state.isOpen}
+          onClose={(e) => this.setState({ isOpen: false })}
+          onNewGame={this.onNewGame}
+        ></Dialog>
+
+        <Game
+          rows={this.state.rows}
+          columns={this.state.columns}
+          mines={this.state.mines}
+          flags={this.state.flags}
+        />
       </div>
     );
   }
