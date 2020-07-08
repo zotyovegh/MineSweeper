d6012a2e95a0b071c9460cb5522fb5b070df50b2
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-07-08 18:15:56 +0200

    Dialogfix and design

--- src/Dialog/Dialog.js
+++ src/Dialog/Dialog.js
@@ -5,65 +5,68 @@ class Dialog extends Component {
   render() {
     let dialog = (
       <div className="dialogStyles">
-        <tbody>
-          <tr>
-            <td>Game</td>
-            <td colSpan="3">
-              <button className="closeButton" onClick={this.props.onClose}>
-                x
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td></td>
-            <td>Height</td>
-            <td>Width</td>
-            <td>Mines</td>
-          </tr>
-          <tr>
-            <td>
-              <input type="radio" value="beginner" name="hardness" /> Beginner
-            </td>
-            <td>9</td>
-            <td>9</td>
-            <td>10</td>
-          </tr>
+        <table>
+          <tbody>
+            <tr>
+              <td id="tableTitle">Game</td>
+              <td colSpan="3">
+                <button className="closeButton" onClick={this.props.onClose}>
+                  x
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Height</td>
+              <td>Width</td>
+              <td>Mines</td>
+            </tr>
+            <tr>
+              <td>
+                <input type="radio" value="beginner" name="hardness" /> Beginner
+              </td>
+              <td>9</td>
+              <td>9</td>
+              <td>10</td>
+            </tr>
 
-          <tr>
-            <td>
-              <input type="radio" value="intermediate" name="hardness" />{" "}
-              Intermediate
-            </td>
-            <td>16</td>
-            <td>16</td>
-            <td>40</td>
-          </tr>
+            <tr>
+              <td>
+                <input type="radio" value="intermediate" name="hardness" />{" "}
+                Intermediate
+              </td>
+              <td>16</td>
+              <td>16</td>
+              <td>40</td>
+            </tr>
 
-          <tr>
-            <td>
-              <input type="radio" value="expert" name="hardness" /> Expert
-            </td>
-            <td>16</td>
-            <td>30</td>
-            <td>99</td>
-          </tr>
+            <tr>
+              <td>
+                <input type="radio" value="expert" name="hardness" /> Expert
+              </td>
+              <td>16</td>
+              <td>30</td>
+              <td>99</td>
+            </tr>
 
-          <tr>
-            <td>
-              <input type="radio" value="intermediate" name="hardness" /> Custom
-            </td>
-            <td>
-              <input type="text" value="20" className="custom"></input>
-            </td>
-            <td>
-              <input type="text" value="30" className="custom"></input>
-            </td>
-            <td>
-              <input type="text" value="145" className="custom"></input>
-            </td>
-          </tr>
-        </tbody>
-        <button id="newGame">New Game</button>
+            <tr>
+              <td>
+                <input type="radio" value="intermediate" name="hardness" />{" "}
+                Custom
+              </td>
+              <td>
+                <input type="text" value="20" className="custom"></input>
+              </td>
+              <td>
+                <input type="text" value="30" className="custom"></input>
+              </td>
+              <td>
+                <input type="text" value="145" className="custom"></input>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button id="newGame" onClick={this.props.onNewGame}>New Game</button>
       </div>
     );
 
