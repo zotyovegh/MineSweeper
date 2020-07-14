1e84472b5e4c16a02f10e07c2a84e620d2db399d
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-07-14 19:36:42 +0200

    Different categories visualized

--- src/Highscore/index.js
+++ src/Highscore/index.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./index.css";
 import firebase from "../firebase";
 
-function useTimes() {
+function useTimes(category) {
   const [beginner, setTimes] = useState([]);
 
   useEffect(() => {
     const unsubscribe = firebase
       .firestore()
-      .collection("beginner")
+      .collection(category)
       .orderBy("highscore", "asc")
       .limit(10)
       .onSnapshot((snapshot) => {
@@ -24,22 +24,56 @@ function useTimes() {
 }
 
 const Highscore = () => {
-  const beginner = useTimes();
+  const beginner = useTimes("beginner");
+  const intermediate = useTimes("intermediate");
+  const expert = useTimes("expert");
   return (
-    <div>
-      <ol>
-        {beginner.map((data) => (
-          <li key={data.id}>
-            <div>
-              {data.name}
-              <code>
-                {"  "}
-                {data.highscore}
-              </code>
-            </div>
-          </li>
-        ))}
-      </ol>
+    <div className="main">
+      <div className="category">
+        <ol>
+          {beginner.map((data) => (
+            <li key={data.id}>
+              <div>
+                {data.name}
+                <code>
+                  {"  "}
+                  {data.highscore}
+                </code>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
+      <div className="category">
+        <ol>
+          {intermediate.map((data) => (
+            <li key={data.id}>
+              <div>
+                {data.name}
+                <code>
+                  {"  "}
+                  {data.highscore}
+                </code>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
+      <div className="category">
+        <ol>
+          {expert.map((data) => (
+            <li key={data.id}>
+              <div>
+                {data.name}
+                <code>
+                  {"  "}
+                  {data.highscore}
+                </code>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 };
