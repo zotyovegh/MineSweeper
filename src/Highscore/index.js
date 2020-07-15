156116ebc92d842bd27e337de42d238e5877fee2
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-07-15 17:01:05 +0200

    Small commit. Lines for receiving the 10th fastest time (will be needed for the winning dialog)

--- src/Highscore/index.js
+++ src/Highscore/index.js
@@ -10,7 +10,7 @@ function useTimes(category) {
       .firestore()
       .collection(category)
       .orderBy("highscore", "asc")
-      .limit(5)
+      .limit(10)
       .onSnapshot((snapshot) => {
         const newData = snapshot.docs.map((doc) => ({
           id: doc.id,
@@ -19,7 +19,7 @@ function useTimes(category) {
         setTimes(newData);
       });
     return () => unsubscribe();
-  }, []);
+  }, [category]);
   return cat;
 }
 
@@ -27,6 +27,11 @@ const Highscore = () => {
   const beginner = useTimes("beginner");
   const intermediate = useTimes("intermediate");
   const expert = useTimes("expert");
+
+  const beginnerLast = beginner[9] && beginner[9].highscore;
+  const intermediateLast = intermediate[9] && intermediate[9].highscore;
+  const expertLast = expert[9] && expert[9].highscore;
+
   return (
     <div className="main">
       <div className="category">
