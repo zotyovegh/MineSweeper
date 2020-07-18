59502a04f9277a44465f1dd028ec51a63282d7d1
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-07-18 07:42:46 +0200

    Basics of the description component

---
+++ src/Description/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./index.css";
+
+class Description extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    let description = <div className="descriptionStyles"></div>;
+
+    if (!this.props.isOpen) {
+      description = null;
+    }
+    return <div>{description}</div>;
+  }
+}
+export default Description;
