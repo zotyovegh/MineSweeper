d90724f46b6bb69d568182d4b154a04027fe39f0
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2021-01-11 19:07:13 +0100

    Winning reformat #3

--- src/Game/Winningdialog/index.js
+++ src/Game/Winningdialog/index.js
@@ -25,28 +25,24 @@ class WinningDialog extends Component {
           Congratulations, you beat the game in {this.props.time} seconds!!!
         </div>
 
-        {(this.props.limit === -1 || this.props.limit > this.props.time) && (
+        {(this.props.limit !== -1 || this.props.limit > this.props.time) && (
           <div>
-            <input type="text" id="name" onChange={this.onNameChange}></input>
-            <button
-              className="winning__buttons"
-              id="save"
-              onClick={this.onSaveTime}
-            >
+            <input
+              type="text"
+              className="winning__input"
+              onChange={this.onNameChange}
+            ></input>
+            <button className="winning__save" onClick={this.onSaveTime}>
               Save my time
             </button>
           </div>
         )}
 
         <div>
-          <button
-            className="buttons"
-            id="newgame"
-            onClick={this.props.onNewGame}
-          >
+          <button className="winning__newGame" onClick={this.props.onNewGame}>
             New Game
           </button>
-          <button className="buttons" id="cancel" onClick={this.props.onClose}>
+          <button className="winning__cancel" onClick={this.props.onClose}>
             Cancel
           </button>
         </div>
